ff9034f935bd8184a238aa92b95ae38244808ac0
alexis <vauchel.alexis@hotmail.fr>
2021-01-11 13:14:01 +0100

    test

--- projet immobilier 2/vérification.py
+++ projet immobilier 2/vérification.py
@@ -1,31 +1,8 @@
-<<<<<<< Updated upstream
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 23 14:17:17 2020
-
-@author: alexis
-"""
-
-
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 21 14:50:06 2020
-
-@author: DIGIFAB
-"""
-
->>>>>>> Stashed changes
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
 from urllib.request import urlopen
-<<<<<<< Updated upstream
 from peewee_immo import db,Logement
-=======
-from orm_peewee import db,Logement
->>>>>>> Stashed changes
 import random
 import time
 
@@ -45,7 +22,3 @@ def verification():
         else:
             time.sleep(3)
             pass
-<<<<<<< Updated upstream
-        
-=======
->>>>>>> Stashed changes
